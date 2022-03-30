734bbd91f0b367dfc5275587af36134170496adb
Alex Buckwald <alexbuckwald@Alexs-MacBook-Air.local>
2022-03-30 11:44:07 +0100

    added humidity to weather forecast

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -1,5 +1,12 @@
 const request = require('request')
 
+// Include new data
+// Update forecast string
+// Commit changes
+// Push Changes to Github and Deploy
+// Test your work in the live application
+
+
 const forecast = (latitude,longitude, callback) => {
 	
 	const url = 'http://api.weatherstack.com/current?access_key=6b75565ca4b4f6dc747336b9bf67c234&query='+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)
@@ -10,7 +17,7 @@ const forecast = (latitude,longitude, callback) => {
 		}else if(body.error){
 			callback(body.error.code, body.error.info,undefined)
 		}else{
-			callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out, it feels like ' + body.current.feelslike+' degrees out')
+			callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out, it feels like ' + body.current.feelslike+' degrees out' + ' and the humidity is ' + body.current.humidity+ "%")
 		}
 	}
 	)
